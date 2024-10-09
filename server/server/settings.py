2bae28b9e207d66bd198512258e5295820b34bc8
dee-at-weaviate <deepti@weaviate.io>
2024-10-09 13:51:05 +0100

    README page update

--- server/server/settings.py
+++ server/server/settings.py
@@ -22,9 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-q3_0#o8s_u9@_*2!4igwyxcz8jy(e#4ulia*5c^vwj70+@)$sy"
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
